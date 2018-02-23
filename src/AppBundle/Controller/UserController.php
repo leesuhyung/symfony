ef392b69eca11d81a2c172e3e33aa7a541b2dccb
navystyle <navystyle@naver.com>
2018-02-23 18:59:05 +0900

    .

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,20 +5,15 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Board;
 use AppBundle\Entity\User;
 use AppBundle\Form\UserPostType;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 /**
  * Class UserController
  * @package AppBundle\Controller
  */
-class UserController extends Controller
+class UserController extends FOSRestController
 {
     /**
      * @Route("/users", methods={"GET"})
@@ -28,14 +23,20 @@ class UserController extends Controller
         $users = $this->getDoctrine()->getRepository(User::class)
             ->findAll();
 
-        return $this->serialize($users);
+        $view = $this->view($users, 200);
+        return $this->handleView($view);
+
+        /* TODO: https://symfony.com/doc/current/bundles/FOSRestBundle/1-setting_up_the_bundle.html
+
+            Serializer 를 이너블 하기위해 1~3번 항목 적용하기.
+        */
     }
 
 
     /**
      * @Route("/user/{id}", methods={"GET"}, requirements={"id": "\d+"})
      * @param $id
-     * @return JsonResponse
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function showAction($id)
     {
@@ -48,14 +49,15 @@ class UserController extends Controller
             );
         }
 
-        return $this->serialize($user);
+        $view = $this->view($user, 200);
+        return $this->handleView($view);
     }
 
 
     /**
      * @Route("/user", methods={"POST"})
      * @param Request $request
-     * @return JsonResponse
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function postAction(Request $request)
     {
@@ -75,7 +77,8 @@ class UserController extends Controller
         // UserPostType 으로 생성된 폼 default configure option 에 'data_class' = User Entity 로 되어있기 때문에
         // $form->getData 는 User 엔티티의 데이터가 리턴된다.
         $user = $this->get('app.service.user_service')->create($form->getData());
-        return $this->serialize($user);
+        $view = $this->view($user, 200);
+        return $this->handleView($view);
     }
 
 
@@ -83,7 +86,7 @@ class UserController extends Controller
      * @Route("/user/{id}", methods={"PUT"}, requirements={"id": "\d+"})
      * @param User $user
      * @param Request $request
-     * @return JsonResponse
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function putAction(User $user, Request $request)
     {
@@ -102,26 +105,28 @@ class UserController extends Controller
 
         // $user 에 id 가 존재하는경우 update 문이 실행된다.
         $user = $this->get('app.service.user_service')->update($form->getData());
-        return $this->serialize($user);
+        $view = $this->view($user, 200);
+        return $this->handleView($view);
     }
 
 
     /**
      * @Route("/user/{id}", methods={"DELETE"}, requirements={"id": "\d+"})
      * @param User $user
-     * @return JsonResponse
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function deleteAction(User $user)
     {
         $this->get('app.service.user_service')->delete($user);
-        return $this->serialize($user);
+        $view = $this->view($user, 200);
+        return $this->handleView($view);
     }
 
 
     /**
      * @Route("/user/{id}/boards", methods={"GET"}, requirements={"id": "\d+"})
      * @param User $user
-     * @return JsonResponse
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function listBoardsAction(User $user)
     {
@@ -136,27 +141,7 @@ class UserController extends Controller
             );
         }
 
-        return $this->serialize($boards);
+        $view = $this->view($boards, 200);
+        return $this->handleView($view);
     }
-
-
-    /**
-     * @param $entity
-     * @return JsonResponse
-     */
-    public function serialize($entity)
-    {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
-        $jsonContent = $serializer->serialize($entity, 'json');
-
-        return new JsonResponse(
-            json_decode($jsonContent)
-        );
-    }
-
-    // TODO: 3. FOS 사용하기 +Object Manager 사용하기
 }
